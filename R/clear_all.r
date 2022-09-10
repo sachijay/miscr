0f4d4581c6a00c84d2f0315ea78d8584759bb03f
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2022-09-10 14:15:34 -0700

    Added clear_all function

---
+++ R/clear_all.r
@@ -0,0 +1,27 @@
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++##
+##                                                            ##
+## Clears the workspace, shuts down all open graphics devices ##
+## and clears the console.                                    ##
+##                                                            ##
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++##
+
+
+#' Clears the workspace, console and shuts down all open graphics devices.
+#'
+#' @export
+#'
+clear_all <- function(){
+  
+  ## Remove all variables from the workspace
+  rm(list = ls())
+  
+  
+  ## Remove all figures
+  graphics.off()
+  
+  
+  ## Clear console
+  cat("\014")
+  
+  
+}
